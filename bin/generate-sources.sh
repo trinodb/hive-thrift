76f668b06c4d7d741d49205fb6a7dfd37d14d93c
David Phillips <david@acz.org>
2023-01-26 21:43:32 -0800

    Fix CI build

--- bin/generate-sources.sh
+++ bin/generate-sources.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-set -eux
+set -eu
 
 cd "${BASH_SOURCE%/*}/.."
 
-SOURCE_DIR=src/main/thrift
-TARGET_DIR=target/generated-sources/thrift
-
-rm -rf $TARGET_DIR
-mkdir -p $TARGET_DIR
+TARGET=/build/target/generated-sources/thrift
 
 # use unstable for Thrift 0.17.0
 docker run -v "${PWD}:/build" --rm debian:unstable /bin/sh -c "\
-apt-get update -q && \
-apt-get install -q -y thrift-compiler && \
-thrift -o /build/${TARGET_DIR} -r \
+set -eux
+apt-get update -q
+apt-get install -q -y thrift-compiler
+rm -rf $TARGET
+mkdir -p $TARGET
+thrift -o $TARGET \
   --gen java:private-members,fullcamel,generated_annotations=undated \
-  /build/${SOURCE_DIR}/hive_metastore.thrift"
-
-mv $TARGET_DIR/gen-java/io ${TARGET_DIR}
-rmdir $TARGET_DIR/gen-java
+  /build/src/main/thrift/hive_metastore.thrift
+mv $TARGET/gen-java/io $TARGET
+rmdir $TARGET/gen-java
+chown -R $UID /build/target
+"

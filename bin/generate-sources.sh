d8de6a3cff07dc9ad8bec9a5a898f9740ce54889
David Phillips <david@acz.org>
2023-10-02 11:20:05 -0700

    Update to Thrift 0.19.0

--- bin/generate-sources.sh
+++ bin/generate-sources.sh
@@ -6,7 +6,7 @@ cd "${BASH_SOURCE%/*}/.."
 
 TARGET=/build/target/generated-sources/thrift
 
-# use unstable for Thrift 0.17.0
+# use unstable for Thrift 0.19.0
 docker run -v "${PWD}:/build" --rm debian:unstable /bin/sh -c "\
 set -eux
 apt-get update -q
@@ -14,7 +14,7 @@ apt-get install -q -y thrift-compiler
 rm -rf $TARGET
 mkdir -p $TARGET
 thrift -o $TARGET \
-  --gen java:private-members,fullcamel,generated_annotations=undated \
+  --gen java:private_members,fullcamel,sorted_containers,generated_annotations=suppress \
   /build/src/main/thrift/hive_metastore.thrift
 mv $TARGET/gen-java/io $TARGET
 rmdir $TARGET/gen-java

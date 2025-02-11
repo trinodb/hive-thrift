354257f62d7a4d523b3a63e79b54dcf9aa451045
okumin <git@okumin.com>
2025-02-11 16:22:14 +0900

    Update the Thrift definition to 4.0.1

--- bin/generate-sources.sh
+++ bin/generate-sources.sh
@@ -5,12 +5,43 @@ set -eu
 cd "${BASH_SOURCE%/*}/.."
 
 TARGET=/build/target/generated-sources/thrift
+GRADLE_VERSION=8.4
+THRIFT_VERSION=$(mvn -q help:evaluate -Dexpression=libthrift.version -DforceStdout)
 
-# use unstable for Thrift 0.19.0
-docker run -v "${PWD}:/build" --rm debian:unstable /bin/sh -c "\
+docker run -v "${PWD}:/build" --rm debian:bookworm /bin/sh -c "\
 set -eux
 apt-get update -q
-apt-get install -q -y thrift-compiler
+apt-get install -q -y \
+  ant \
+  automake\
+  bison \
+  flex \
+  g++ \
+  git \
+  libboost-all-dev \
+  libevent-dev \
+  libssl-dev \
+  libtool \
+  make \
+  openjdk-17-jdk-headless \
+  pkg-config \
+  unzip \
+  wget
+
+wget https://services.gradle.org/distributions/gradle-$GRADLE_VERSION-bin.zip -q \
+  -O /tmp/gradle-$GRADLE_VERSION-bin.zip
+unzip -d /tmp /tmp/gradle-$GRADLE_VERSION-bin.zip
+mv /tmp/gradle-$GRADLE_VERSION /usr/local/gradle
+ln -s /usr/local/gradle/bin/gradle /usr/local/bin
+
+wget 'https://dlcdn.apache.org/thrift/$THRIFT_VERSION/thrift-$THRIFT_VERSION.tar.gz'
+tar zxf thrift-$THRIFT_VERSION.tar.gz
+cd thrift-$THRIFT_VERSION
+./bootstrap.sh
+./configure --without-cpp --without-kotlin --without-python --without-py3
+make
+make install
+
 rm -rf $TARGET
 mkdir -p $TARGET
 thrift -o $TARGET \
